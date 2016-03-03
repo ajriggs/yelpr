9e027844a61e9ebd2434edd7b1655c5908391a4c
Aaron Riggs <riggs.aaron@gmail.com>
2016-03-03 13:13:35 -0800

    optimized db seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+require 'fabrication'
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
@@ -6,7 +8,12 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-  nintendo = Business.create name: 'Nintendo'
-  docker = Business.create name: 'Docker'
-  apple = Business.create name: 'Apple'
-  samsung = Business.create name: 'Samsung'
+# nintendo = Business.create name: 'Nintendo'
+# docker = Business.create name: 'Docker'
+# apple = Business.create name: 'Apple'
+# samsung = Business.create name: 'Samsung'
+
+nintendo_review = Fabricate :review, business: Fabricate(:business, name: "Nintendo"), user: Fabricate(:user)
+docker_review = Fabricate :review, business: Fabricate(:business, name: "Docker"), user: Fabricate(:user)
+apple_review = Fabricate :review, business: Fabricate(:business, name: "Apple"), user: Fabricate(:user)
+samsung_review = Fabricate :review, business: Fabricate(:business, name: "Samsung"), user: Fabricate(:user)
